1e762be2119ed0400b4b93c1fc426698d71b3398
Mykhailo Muravynets <mykhailo.muravynets@socialtechnologies.io>
2022-02-18 16:24:16 +0200

    MLinkedListEnumerator

---
+++ MMS.LinkedList/src/Internal/MLinkedListEnumerator.cs
@@ -0,0 +1,59 @@
+using System.Collections;
+using System.Runtime.Serialization;
+using MMS.LinkedList.Abstract;
+
+namespace MMS.LinkedList.Internal;
+
+public class MLinkedListEnumerator<T> :
+    IEnumerator<T>,
+    ISerializable,
+    IDeserializationCallback
+{
+    private MLinkedListNode<T> _current;
+    private readonly MLinkedList<T> _list;
+    private readonly MLinkedListNode<T> _head;
+
+
+    public MLinkedListEnumerator(MLinkedList<T> list, MLinkedListNode<T> head)
+    {
+        _list = list;
+        _head = head;
+        Reset();
+    }
+
+    public bool MoveNext()
+    {
+        if (_current.Next is null)
+            return false;
+
+        _current = _current.Next;
+
+        return true;
+    }
+
+    public void Reset()
+    {
+        _current = new MLinkedListNode<T>(default(T))
+        {
+            Next = _head
+        };
+    }
+
+    public T Current => _current.Item;
+
+    object IEnumerator.Current => Current;
+
+    public void Dispose()
+    {
+    }
+
+    public void GetObjectData(SerializationInfo info, StreamingContext context)
+    {
+        throw new PlatformNotSupportedException();
+    }
+
+    public void OnDeserialization(object? sender)
+    {
+        throw new PlatformNotSupportedException();
+    }
+}
